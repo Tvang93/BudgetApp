7d7862c3d1bdbbb7a81022296339b5720a43576a
Tvang93 <vangt39@gmail.com>
2025-02-10 11:38:22 -0800

    made modals

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,20 @@
+const totalIncome = document.getElementById("totalIncome");
 const incomeList = document.getElementById("incomeList");
 const addIncomeBtn = document.getElementById("addIncomeBtn");
+const totalExpense = document.getElementById("totalExpense");
 const expenseList = document.getElementById("expenseList");
-const addExpensesBtn = document.getElementById("addExpensesBtn");
+const addExpenseBtn = document.getElementById("addExpenseBtn");
 const myBudget = document.getElementById("myBudget");
+
+const addIncomeModal = document.getElementById("addIncomeModal");
+const closeAddIncomeBtn = document.getElementById("closeAddIncomeBtn");
+const incomeTitle = document.getElementById("incomeTitle");
+const incomeValue = document.getElementById("incomeValue");
+const confirmIncomeBtn = document.getElementById("confirmIncomeBtn");
+
+const addExpenseModal = document.getElementById("addExpenseModal");
+const closeAddExpenseBtn = document.getElementById("closeAddExpenseBtn");
+const expenseTitle = document.getElementById("expenseTitle");
+const expenseValue = document.getElementById("expenseValue");
+const confirmExpenseBtn = document.getElementById("confirmExpenseBtn");
 
