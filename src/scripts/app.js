207d83d803b085492c424f5d0e9001048b2616e8
Tvang93 <vangt39@gmail.com>
2025-02-10 10:31:46 -0800

    initial commit

---
+++ src/scripts/app.js
@@ -0,0 +1,6 @@
+const incomeList = document.getElementById("incomeList");
+const addIncomeBtn = document.getElementById("addIncomeBtn");
+const expenseList = document.getElementById("expenseList");
+const addExpensesBtn = document.getElementById("addExpensesBtn");
+const myBudget = document.getElementById("myBudget");
+
